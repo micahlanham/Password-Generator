da9f9c6299248a435664b5e86140d7a7f78d793c
micahlanham <84043453+micahlanham@users.noreply.github.com>
2021-07-10 21:31:58 -0400

    Update script.js

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,5 +1,33 @@
 // Assignment code here
+function generatePassword() {
+    var numbers = ['0123456789'];
+    var upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+    var lowerCase = ['abcdefghijklmnopqrstuvwxyz']
+    var specialCharacters = ['!@#$%^&*()']
 
+    var numQuestion = window.prompt('How many characters do you want your password to have?');
+    
+    var upCaseQuestion = window.confirm('Would you like to include Upper Case Letters?');
+    if(upCaseQuestion){
+        finalPassword = finalPassword(upperCase);
+    }
+    var lowCaseQuestion = window.confirm('Would you like to include Lower Case Letters?');
+    if(lowCaseQuestion){
+        finalPassword = finalPassword(lowerCase);
+    }
+    var specialCharactersQuestion = window.confirm('Would you like to include special characters?');
+    if(specialCharactersQuestion){
+        finalPassword = finalPassword(specialCharacters);
+    }
+
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
